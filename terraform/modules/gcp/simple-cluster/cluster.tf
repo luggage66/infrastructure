d2dbb50a082c353dd32c88f395038574dc0f365d
Donald Mull Jr <donald.mull@gmail.com>
2020-02-07 17:55:07 -0500

    fix: give cluster access to pull docker images

--- terraform/modules/gcp/simple-cluster/cluster.tf
+++ terraform/modules/gcp/simple-cluster/cluster.tf
@@ -39,6 +39,7 @@ resource "google_container_node_pool" "primary_pool" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only"
     ]
   }
 }
